530eee5167e8caaf645b1db1acf5b04f4fe7f142
Flavianna Saldanha <flaviannasaldanha@gmail.com>
2026-01-22 15:29:27 -0300

    feat: finalizing `menu` adjustments

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import { Outlet } from "react-router-dom";
 import ButtonAppBar from "./components/menu/Menu";
 
 function App() {
--- src/components/menu/Menu.tsx
+++ src/components/menu/Menu.tsx
@@ -2,31 +2,85 @@ import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
-import Button from "@mui/material/Button";
-import IconButton from "@mui/material/IconButton";
-import MenuIcon from "@mui/icons-material/Menu";
+import MovieIcon from "@mui/icons-material/Movie";
+import { Avatar } from "@mui/material";
 
 export default function ButtonAppBar() {
 	return (
 		<Box sx={{ flexGrow: 1 }}>
-			<AppBar position="static"
-				sx={{ backgroundColor: "var(--bg-primary)",
-					borderBottom: "1px solid var(--text-secondary)",
-				 }}>
-				<Toolbar>
-					<IconButton
-						size="large"
-						edge="start"
-						color="inherit"
-						aria-label="menu"
-						sx={{ mr: 2 }}
+			<AppBar
+				position="static"
+				sx={{
+					backgroundColor: "var(--bg-primary)",
+					borderBottom: "1px solid var(--text-tertiary)",
+					px: { xs: 0, sm: 10 },
+				}}
+			>
+				<Toolbar sx={{ display: "flex", alignItems: "center", gap: 2 }}>
+					<Box sx={{ display: "flex", alignItems: "center", gap: 1 }}>
+						<Box
+							sx={{
+								backgroundColor: "var(--bg-tertiary)",
+								paddingX: "7px",
+								paddingY: "4px",
+								borderRadius: "5px",
+								display: "flex",
+								alignItems: "center",
+								justifyContent: "center",
+							}}
+						>
+							<MovieIcon sx={{ color: "#fff" }} />
+						</Box>
+
+						<Typography variant="h6" sx={{ fontWeight: "bold" }}>
+              Movie App
+						</Typography>
+					</Box>
+
+					<Box
+						sx={{
+							display: "flex",
+							gap: 1,
+							backgroundColor: "var(--bg-primary)",
+							padding: "8px",
+							borderRadius: 2,
+							flexGrow: 1,
+							alignItems: "center",
+						}}
 					>
-						<MenuIcon />
-					</IconButton>
-					<Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            Movie App
-					</Typography>
-					<Button color="inherit">Login</Button>
+						{["Movies", "TV Shows", "Watchlist"].map((item, index) => (
+							<Box
+								key={item}
+								sx={{
+									padding: "6px 12px",
+									borderRadius: 1,
+									display: { xs: "none", md: "flex" }		,							
+									alignItems: "center",
+									justifyContent: "center",
+									cursor: "pointer",
+								}}
+							>
+								<Typography
+									variant="body1"
+									sx={{
+										fontWeight: "bold",
+										fontSize: 14,
+										color:
+                      index === 0
+                      	? "var(--text-primary)"
+                      	: "var(--text-secondary)",
+										"&:hover": {
+											color: "var(--bg-tertiary)",
+										},
+									}}
+								>
+									{item}
+								</Typography>
+							</Box>
+						))}
+					</Box>
+
+					<Avatar alt="Agnes Walker" src="" />
 				</Toolbar>
 			</AppBar>
 		</Box>
